622bc29a14b5c5a0ae82a71fdd638330609311ea
Thenukad Dinu <thenukadinu@gmail.com>
2022-09-30 00:12:33 +0530

    development commit - 01

---
+++ src/components/MenuNavbar.vue
@@ -0,0 +1,25 @@
+<script setup>
+import logoSVG from "@/assets/images/logo.svg";
+</script>
+<template>
+  <div class="menu-navbar">
+    <div class="logo">
+      <img alt="jamex-main-logo" :src="logoSVG" />
+    </div>
+    <ul class="menus-group">
+      <li>Option 1</li>
+      <li>Option 2</li>
+      <li>Option 3</li>
+      <li>Option 4</li>
+      <li>Option 5</li>
+      <li>Option 6</li>
+    </ul>
+    <div class="buttons-group">
+      <div class="login button">Log in</div>
+      <div class="register button">Register</div>
+    </div>
+  </div>
+</template>
+<style scoped lang="scss">
+@import "@/assets/scss/menuNavbar.scss";
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,16 @@
-<script setup></script>
+<script setup>
+import MenuNavbar from "../components/MenuNavbar.vue";
+</script>
 
 <template>
   <div class="home">
-    <h1>This is home</h1>
+    <div class="top">
+      <div class="right-fade"></div>
+      <MenuNavbar class="menu-navbar" />
+    </div>
   </div>
 </template>
+
+<style scoped lang="scss">
+@import "@/assets/scss/homeView.scss";
+</style>
