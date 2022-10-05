1cff35078f274d6a7408629a91b6a7c3a8a5df76
Madushan Wijesuriya <67863463+madushanWijesuriya@users.noreply.github.com>
2022-10-05 23:42:08 +0530

    added card one component

---
+++ src/components/Cards/CardOne.vue
@@ -0,0 +1,16 @@
+<script setup></script>
+<template>
+  <div class="card">
+    <div class="cardBatch"></div>
+
+    <!-- <img class="card-img-top" src="..." alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div> -->
+  </div>
+</template>
+<style scoped lang="scss">
+@import "@/assets/scss/cardOne";
+</style>
---
+++ src/views/TestView.vue
@@ -0,0 +1,7 @@
+<script setup>
+import CardOne from "../components/Cards/CardOne.vue";
+</script>
+<template>
+  <CardOne />
+</template>
+<style></style>
