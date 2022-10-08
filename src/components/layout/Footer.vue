b96866909430ac344944786dca33546e94bb7429
Madushan Wijesuriya <67863463+madushanWijesuriya@users.noreply.github.com>
2022-10-09 00:13:56 +0530

    add footer changes

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -1,6 +1,349 @@
 <script setup></script>
 <template>
-  <div class="footer">This is footer</div>
+  <div class="footer">
+    <div class="container mx-auto">
+      <div
+        class="grid grid-cols-10 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-10 xl:grid-cols-10 gap-7 m-12 sm:m-2"
+      >
+        <div
+          class="col-span-2 pt-12 sm:col-span-10 md:col-span-2 lg:col-span-2 xl:col-span-2"
+        >
+          <div class="flex sm:justify-center">
+            <img class="mb-10" src="/public/footer-image-1.svg" />
+          </div>
+          <h1
+            class="text-left sm:text-center md:text-left lg:text-left xl:text-left text-white footer-contect-text"
+          >
+            Jamex Auction House will give you due assistance regarding the
+            procedure with which you can place your bids and control them for
+            the deposit, auction,and payments
+          </h1>
+        </div>
+        <div class="col-span-8">
+          <div
+            class="grid grid-cols-8 sm:grid-cols-8 md:grid-cols-8 lg:grid-cols-8 xl:grid-cols-8 gap-7"
+          >
+            <div
+              class="col-span-1 sm:col-span-4 md:col-span-4 lg:col-span-4 xl:col-span-1 pt-12"
+            >
+              <h1 class="text-left footer-topic">About Us</h1>
+              <br />
+              <div class="mb-20">
+                <div class="mb-5">
+                  <a
+                    href="#"
+                    class="text-left text-white footer-contect-text underline"
+                    >Why Choose Us?
+                  </a>
+                </div>
+                <div class="mb-5">
+                  <a
+                    href="#"
+                    class="text-left text-white footer-contect-text underline"
+                    >Company Profile
+                  </a>
+                </div>
+
+                <div class="mb-5">
+                  <a
+                    href="#"
+                    class="text-left text-white footer-contect-text underline"
+                    >Corporate Philosophy
+                  </a>
+                </div>
+
+                <div class="mb-5">
+                  <a
+                    href="#"
+                    class="text-left text-white footer-contect-text underline"
+                    >Quality Control
+                  </a>
+                </div>
+
+                <div class="mb-5">
+                  <a
+                    href="#"
+                    class="text-left text-white footer-contect-text underline"
+                    >Customer Reviews
+                  </a>
+                </div>
+
+                <div class="mb-5">
+                  <a
+                    href="#"
+                    class="text-left text-white footer-contect-text underline"
+                    >Contact Us</a
+                  >
+                </div>
+              </div>
+            </div>
+            <div
+              class="col-span-2 sm:col-span-4 md:col-span-4 lg:col-span-4 xl:col-span-2 pt-12 sm:pl-5 md:pl-5 lg:pl-20 xl:pl-20"
+            >
+              <h1 class="text-left footer-topic">Auctions</h1>
+              <br />
+              <div class="mb-20">
+                <div class="mb-5">
+                  <a
+                    href="#"
+                    class="text-left text-white footer-contect-text underline"
+                    >Auction Calendar
+                  </a>
+                </div>
+                <div class="mb-5">
+                  <a
+                    href="#"
+                    class="text-left text-white footer-contect-text underline"
+                    >Auction Houses
+                  </a>
+                </div>
+
+                <div class="mb-5">
+                  <a
+                    href="#"
+                    class="text-left text-white footer-contect-text underline"
+                    >Auction Guide
+                  </a>
+                </div>
+
+                <div class="mb-5">
+                  <a
+                    href="#"
+                    class="text-left text-white footer-contect-text underline"
+                    >Auction Vs Stock
+                  </a>
+                </div>
+
+                <div class="mb-5">
+                  <a
+                    href="#"
+                    class="text-left text-white footer-contect-text underline"
+                    >How to order
+                  </a>
+                </div>
+
+                <div class="mb-5">
+                  <a
+                    href="#"
+                    class="text-left text-white footer-contect-text underline"
+                    >Latest News</a
+                  >
+                </div>
+                <div class="mb-5">
+                  <a
+                    href="#"
+                    class="text-left text-white footer-contect-text underline"
+                    >FAQ</a
+                  >
+                </div>
+              </div>
+            </div>
+            <div
+              class="col-span-3 sm:col-span-8 md:col-span-8 lg:col-span-3 xl:col-span-3 pt-12"
+            >
+              <div class="">
+                <h1
+                  class="footer-topic sm:text-center md:text-center lg:text-left lx:text-left"
+                >
+                  Support over the world
+                </h1>
+                <br />
+                <div>
+                  <div
+                    class="grid grid-rows-7 sm:grid-rows-11 md:grid-rows-11 lg:grid-rows-11 xl:grid-rows-11 grid-flow-col gap-4 sm:gap-6"
+                  >
+                    <div>
+                      <h1 class="text-white footer-contect-text">
+                        <img
+                          class="inline mr-2"
+                          src="public\footer\flags\twemoji_flag-bangladesh.svg"
+                        />Bangladesh
+                      </h1>
+                    </div>
+                    <div>
+                      <h1 class="text-white footer-contect-text">
+                        <img
+                          class="inline mr-2"
+                          src="public\footer\flags\emojione-v1_flag-for-malawi.svg"
+                        />Malawi
+                      </h1>
+                    </div>
+                    <div>
+                      <h1 class="text-white footer-contect-text">
+                        <img
+                          class="inline mr-2"
+                          src="public\footer\flags\emojione-v1_flag-for-trinidad-and-tobago.svg"
+                        />Trinidad & Tobago
+                      </h1>
+                    </div>
+                    <div>
+                      <h1 class="text-white footer-contect-text">
+                        <img
+                          class="inline mr-2"
+                          src="public\footer\flags\emojione-v1_flag-for-botswana.svg"
+                        />Botswana
+                      </h1>
+                    </div>
+                    <div>
+                      <h1 class="text-white footer-contect-text">
+                        <img
+                          class="inline mr-2"
+                          src="public\footer\flags\emojione-v1_flag-for-malta.svg"
+                        />Malta
+                      </h1>
+                    </div>
+                    <div>
+                      <h1 class="text-white footer-contect-text">
+                        <img
+                          class="inline mr-2"
+                          src="public\footer\flags\emojione-v1_flag-for-tanzania.svg"
+                        />Tanzania
+                      </h1>
+                    </div>
+                    <div>
+                      <h1 class="text-white footer-contect-text">
+                        <img
+                          class="inline mr-2"
+                          src="public\footer\flags\emojione-v1_flag-for-congo-brazzaville.svg"
+                        />DR Congo
+                      </h1>
+                    </div>
+                    <div>
+                      <h1 class="text-white footer-contect-text">
+                        <img
+                          class="inline mr-2"
+                          src="public\footer\flags\emojione-v1_flag-for-new-zealand.svg"
+                        />New Zealand
+                      </h1>
+                    </div>
+                    <div>
+                      <h1 class="text-white footer-contect-text">
+                        <img
+                          class="inline mr-2"
+                          src="public\footer\flags\emojione-v1_flag-for-turkey.svg"
+                        />Turkey
+                      </h1>
+                    </div>
+                    <div>
+                      <h1 class="text-white footer-contect-text">
+                        <img
+                          class="inline mr-2"
+                          src="public\footer\flags\twemoji_flag-cyprus.svg"
+                        />Cyprus
+                      </h1>
+                    </div>
+                    <div>
+                      <h1 class="text-white footer-contect-text">
+                        <img
+                          class="inline mr-2"
+                          src="public\footer\flags\emojione-v1_flag-for-pakistan.svg"
+                        />Pakistan
+                      </h1>
+                    </div>
+                    <div>
+                      <h1 class="text-white footer-contect-text">
+                        <img
+                          class="inline mr-2"
+                          src="public\footer\flags\emojione-v1_flag-for-myanmar.svg"
+                        />Myanmar
+                      </h1>
+                    </div>
+                    <div>
+                      <h1 class="text-white footer-contect-text">
+                        <img
+                          class="inline mr-2"
+                          src="public\footer\flags\emojione-v1_flag-for-fiji.svg"
+                        />Fiji
+                      </h1>
+                    </div>
+                    <div>
+                      <h1 class="text-white footer-contect-text">
+                        <img
+                          class="inline mr-2"
+                          src="public\footer\flags\emojione-v1_flag-for-russia.svg"
+                        />Russia
+                      </h1>
+                    </div>
+                    <div>
+                      <h1 class="text-white footer-contect-text">
+                        <img
+                          class="inline mr-2"
+                          src="public\footer\flags\emojione-v1_flag-for-uganda.svg"
+                        />Uganda
+                      </h1>
+                    </div>
+                    <div>
+                      <h1 class="text-white footer-contect-text">
+                        <img
+                          class="inline mr-2"
+                          src="public\footer\flags\emojione-v1_flag-for-jamaica.svg"
+                        />Jamaica
+                      </h1>
+                    </div>
+                    <div>
+                      <h1 class="text-white footer-contect-text">
+                        <img
+                          class="inline mr-2"
+                          src="public\footer\flags\emojione-v1_flag-for-sri-lanka.svg"
+                        />Srilanka
+                      </h1>
+                    </div>
+                    <div>
+                      <h1 class="text-white footer-contect-text">
+                        <img
+                          class="inline mr-2"
+                          src="public\footer\flags\emojione-v1_flag-for-united-kingdom.svg"
+                        />United Kingdom
+                      </h1>
+                    </div>
+                    <div>
+                      <h1 class="text-white footer-contect-text">
+                        <img
+                          class="inline mr-2"
+                          src="public\footer\flags\emojione-v1_flag-for-kenya.svg"
+                        />Kenya
+                      </h1>
+                    </div>
+                    <div>
+                      <h1 class="text-white footer-contect-text">
+                        <img
+                          class="inline mr-2"
+                          src="public\footer\flags\emojione-v1_flag-for-haiti.svg"
+                        />Haiti
+                      </h1>
+                    </div>
+                    <div>
+                      <h1 class="text-white footer-contect-text">
+                        <img
+                          class="inline mr-2"
+                          src="public\footer\flags\emojione-v1_flag-for-zambia.svg"
+                        />Zambia
+                      </h1>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-span-2 pt-12 ml-10">
+              <h1 class="footer-topic">Reach us</h1>
+              <br />
+              <h1 class="text-white footer-contect-text">
+                <img
+                  class="inline mr-2"
+                  src="public\footer\logos_whatsapp-icon.svg"
+                />+81 3 6712 4147
+              </h1>
+              <br />
+              <img
+                class="mr-2"
+                src="public\footer\QR_code_for_mobile_English_Wikipedia 1.svg"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <style scoped lang="scss">
 @import "@/assets/scss/footer.scss";
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,20 @@
 module.exports = {
   content: ["./public/**/*.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateRows: {
+        // Simple 8 row grid
+        7: "repeat(7, minmax(0, 1fr))",
+        8: "repeat(8, minmax(0, 1fr))",
+        9: "repeat(9, minmax(0, 1fr))",
+        10: "repeat(10, minmax(0, 1fr))",
+        11: "repeat(11, minmax(0, 1fr))",
+
+        // Complex site-specific row configuration
+      },
+    },
     screens: {
-      sm: "600px",
+      sm: "360px",
       // => @media (min-width: 640px) { ... }
 
       md: "768px",
