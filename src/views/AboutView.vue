51fcc1ec5206bae35352670f81b882ee50868c6a
unknown <lakweensenathilake@gmail.com>
2022-10-31 22:08:07 +0530

    dev

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -13,30 +13,40 @@ var base_url = window.location.origin;
         </svg>
       </div>
       <div class="flex items-center">
-        <div class="lg:w-[150px] xl:w-[200px] md:w-[120px]">
+        <div class="lg:w-[150px] xl:w-[250px] md:w-[120px]">
           <img class="w-full" src="@/assets/images/logo.svg" />
         </div>
 
       </div>
       <div
-        class="hidden md:flex flex-row md:gap-7 xl:gap-13 items-center font-semibold md:text-[12px] lg:text-[15px] xl:text-[20px]">
+        class="hidden md:flex flex-row md:gap-4 xl:gap-[2vw] items-center font-semibold md:text-[12px] lg:text-[15px] xl:text-[20px]">
         <div>
           <a class="font-semibold" href="http://127.0.0.1:5173/">Home</a>
         </div>
-        <div>
-          <a class="font-semibold" href="http://127.0.0.1:5173/service">Service</a>
+        <div class="flex items-center gap-2">
+          <a class="font-semibold" href="http://127.0.0.1:5173/service">Services</a>
+          <div class="w-[10px] h-[10px]">
+            <svg width="100%" height="100%" viewBox="0 0 14 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M1 1L7 7L13 1" stroke="#08246C" stroke-width="1.5" stroke-linecap="round" />
+            </svg>
+          </div>
         </div>
         <div class="font-semibold">
-          Countries
+          <a class="font-semibold" href="http://127.0.0.1:5173/countries-vehicle-details">Countries</a>
         </div>
         <div class="font-semibold">
           How to Order
         </div>
-        <div class="font-semibold">
+        <div class="flex items-center gap-2">
           <a class="font-semibold" href="http://127.0.0.1:5173/about">About Us</a>
+          <div class="w-[10px] h-[10px]">
+            <svg width="100%" height="100%" viewBox="0 0 14 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M1 1L7 7L13 1" stroke="#08246C" stroke-width="1.5" stroke-linecap="round" />
+            </svg>
+          </div>
         </div>
         <div class="font-semibold">
-          Reach Us
+          <a class="font-semibold" href="http://127.0.0.1:5173/reachus">Reach Us</a>
         </div>
       </div>
       <div class="hidden md:flex lg:flex flex-row gap-5 items-center justify-center">
@@ -45,19 +55,21 @@ var base_url = window.location.origin;
           Log in
         </div>
         <div
-          class="rounded bg-[#08246C] px-5 p-1 md:text-[12px] lg:text-[15px] xl:text-[20px] text-white lg:p-2 lg:px-8 xl:px-10">
+          class="rounded xl:rounded-lg bg-[#08246C] px-5 p-1 md:text-[12px] lg:text-[15px] xl:text-[20px] text-white lg:p-2 lg:px-8 xl:px-[3vw] xl:py-4">
           Register
         </div>
       </div>
       <div class="visible flex items-center md:hidden">
         <svg width="18" height="19" viewBox="0 0 18 19" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M17 0H1C0.734784 0 0.48043 0.105357 0.292893 0.292893C0.105357 0.48043 0 0.734784 0 1V3.59C0 4.113 0.213 4.627 0.583 4.997L6 10.414V18C6.0002 18.1704 6.04387 18.3379 6.1269 18.4867C6.20992 18.6354 6.32955 18.7605 6.47444 18.8502C6.61934 18.9398 6.78471 18.9909 6.9549 18.9988C7.1251 19.0066 7.29447 18.9709 7.447 18.895L11.447 16.895C11.786 16.725 12 16.379 12 16V10.414L17.417 4.997C17.787 4.627 18 4.113 18 3.59V1C18 0.734784 17.8946 0.48043 17.7071 0.292893C17.5196 0.105357 17.2652 0 17 0ZM10.293 9.293C10.2 9.38571 10.1262 9.4959 10.0759 9.61724C10.0256 9.73857 9.99981 9.86866 10 10V15.382L8 16.382V10C8.00019 9.86866 7.9744 9.73857 7.92412 9.61724C7.87383 9.4959 7.80004 9.38571 7.707 9.293L2 3.59V2H16.001L16.003 3.583L10.293 9.293Z" fill="black"/>
+          <path
+            d="M17 0H1C0.734784 0 0.48043 0.105357 0.292893 0.292893C0.105357 0.48043 0 0.734784 0 1V3.59C0 4.113 0.213 4.627 0.583 4.997L6 10.414V18C6.0002 18.1704 6.04387 18.3379 6.1269 18.4867C6.20992 18.6354 6.32955 18.7605 6.47444 18.8502C6.61934 18.9398 6.78471 18.9909 6.9549 18.9988C7.1251 19.0066 7.29447 18.9709 7.447 18.895L11.447 16.895C11.786 16.725 12 16.379 12 16V10.414L17.417 4.997C17.787 4.627 18 4.113 18 3.59V1C18 0.734784 17.8946 0.48043 17.7071 0.292893C17.5196 0.105357 17.2652 0 17 0ZM10.293 9.293C10.2 9.38571 10.1262 9.4959 10.0759 9.61724C10.0256 9.73857 9.99981 9.86866 10 10V15.382L8 16.382V10C8.00019 9.86866 7.9744 9.73857 7.92412 9.61724C7.87383 9.4959 7.80004 9.38571 7.707 9.293L2 3.59V2H16.001L16.003 3.583L10.293 9.293Z"
+            fill="black" />
         </svg>
 
       </div>
     </div>
     <div
-      class="flex flex-col justify-center items-lcenters w-full md:px-[60px] lg:px-[80px] xl:px-[180px] md:mt-2 xl:mt-2">
+      class="flex flex-col justify-center items-lcenters w-full md:px-[60px] lg:px-[80px] xl:px-[180px] md:mt-2 xl:mt-3">
       <div class="relative w-full">
         <img class="lg:rounded-[15px] md:rounded-[10px] w-full" src="@/assets/images/aboutus/aboutus-img-1.svg" />
         <div class="absolute text-white top-1/2 left-1/4 -translate-x-1/4 -translate-y-1/2">
