1232242f0c814782f1bbfa204d3748cf0fe672b1
unknown <lakweensenathilake@gmail.com>
2022-11-02 21:00:47 +0530

    fix

--- src/views/aboutUsCusFeedback.vue
+++ src/views/aboutUsCusFeedback.vue
@@ -4,7 +4,7 @@ var base_url = window.location.origin;
 <template>
     <div class="text-white text-sm">
         <div
-            class="flex flex-col gap-10 justify-center items-lcenters w-full md:px-[60px] lg:px-[80px] xl:px-[180px] md:mt-2 xl:mt-3">
+            class="flex flex-col gap-10 justify-center items-center w-full md:px-[60px] lg:px-[80px] xl:px-[180px] md:mt-2 xl:mt-3">
             <div class="relative w-full">
                 <img class="lg:rounded-[15px] md:rounded-[10px] w-full h-[300px] object-cover"
                     src="@/assets/images/customer-feedback/cus-feedback.svg" />
@@ -27,51 +27,70 @@ var base_url = window.location.origin;
                 <p class="text-center text-[20px] font-nunito font-bold text-[#08246C]">Loved by businesses, and
                     individuals across the globe.</p>
             </div>
-            <div class="border-[1px] border-[#08246C] space-y-5 mx-4 py-5 px-3 rounded-lg">
+            <div
+                class="relative border-[1px] border-[#08246C] w-[380px] h-[350px] flex flex-col gap-2 space-y-5 py-7 px-5 rounded-lg">
                 <div class="flex gap-1">
-                        <svg width="21" height="21" viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path
-                                d="M10.6004 0.779602C10.6726 0.543977 11.0062 0.543976 11.0784 0.779601L13.2408 7.8324C13.2729 7.93742 13.3699 8.00912 13.4798 8.00912H20.5067C20.7454 8.00912 20.8483 8.31161 20.6592 8.45721L14.9498 12.8533C14.8672 12.9169 14.8328 13.025 14.8633 13.1247L17.0382 20.2183C17.1096 20.4512 16.8396 20.6383 16.6466 20.4897L10.9919 16.1357C10.902 16.0665 10.7768 16.0665 10.6869 16.1357L5.0322 20.4897C4.83918 20.6383 4.56926 20.4512 4.64066 20.2183L6.8155 13.1247C6.84605 13.025 6.81158 12.9169 6.729 12.8533L1.01959 8.45721C0.830493 8.31161 0.933455 8.00912 1.17211 8.00912H8.19905C8.30889 8.00912 8.40587 7.93742 8.43806 7.8324L10.6004 0.779602Z"
-                                fill="#FBB040" />
-                        </svg>
+                    <svg width="21" height="21" viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M10.6004 0.779602C10.6726 0.543977 11.0062 0.543976 11.0784 0.779601L13.2408 7.8324C13.2729 7.93742 13.3699 8.00912 13.4798 8.00912H20.5067C20.7454 8.00912 20.8483 8.31161 20.6592 8.45721L14.9498 12.8533C14.8672 12.9169 14.8328 13.025 14.8633 13.1247L17.0382 20.2183C17.1096 20.4512 16.8396 20.6383 16.6466 20.4897L10.9919 16.1357C10.902 16.0665 10.7768 16.0665 10.6869 16.1357L5.0322 20.4897C4.83918 20.6383 4.56926 20.4512 4.64066 20.2183L6.8155 13.1247C6.84605 13.025 6.81158 12.9169 6.729 12.8533L1.01959 8.45721C0.830493 8.31161 0.933455 8.00912 1.17211 8.00912H8.19905C8.30889 8.00912 8.40587 7.93742 8.43806 7.8324L10.6004 0.779602Z"
+                            fill="#FBB040" />
+                    </svg>
 
-                        <svg width="21" height="21" viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path
-                                d="M10.6004 0.779602C10.6726 0.543977 11.0062 0.543976 11.0784 0.779601L13.2408 7.8324C13.2729 7.93742 13.3699 8.00912 13.4798 8.00912H20.5067C20.7454 8.00912 20.8483 8.31161 20.6592 8.45721L14.9498 12.8533C14.8672 12.9169 14.8328 13.025 14.8633 13.1247L17.0382 20.2183C17.1096 20.4512 16.8396 20.6383 16.6466 20.4897L10.9919 16.1357C10.902 16.0665 10.7768 16.0665 10.6869 16.1357L5.0322 20.4897C4.83918 20.6383 4.56926 20.4512 4.64066 20.2183L6.8155 13.1247C6.84605 13.025 6.81158 12.9169 6.729 12.8533L1.01959 8.45721C0.830493 8.31161 0.933455 8.00912 1.17211 8.00912H8.19905C8.30889 8.00912 8.40587 7.93742 8.43806 7.8324L10.6004 0.779602Z"
-                                fill="#FBB040" />
-                        </svg>
+                    <svg width="21" height="21" viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M10.6004 0.779602C10.6726 0.543977 11.0062 0.543976 11.0784 0.779601L13.2408 7.8324C13.2729 7.93742 13.3699 8.00912 13.4798 8.00912H20.5067C20.7454 8.00912 20.8483 8.31161 20.6592 8.45721L14.9498 12.8533C14.8672 12.9169 14.8328 13.025 14.8633 13.1247L17.0382 20.2183C17.1096 20.4512 16.8396 20.6383 16.6466 20.4897L10.9919 16.1357C10.902 16.0665 10.7768 16.0665 10.6869 16.1357L5.0322 20.4897C4.83918 20.6383 4.56926 20.4512 4.64066 20.2183L6.8155 13.1247C6.84605 13.025 6.81158 12.9169 6.729 12.8533L1.01959 8.45721C0.830493 8.31161 0.933455 8.00912 1.17211 8.00912H8.19905C8.30889 8.00912 8.40587 7.93742 8.43806 7.8324L10.6004 0.779602Z"
+                            fill="#FBB040" />
+                    </svg>
 
-                        <svg width="21" height="21" viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path
-                                d="M10.6004 0.779602C10.6726 0.543977 11.0062 0.543976 11.0784 0.779601L13.2408 7.8324C13.2729 7.93742 13.3699 8.00912 13.4798 8.00912H20.5067C20.7454 8.00912 20.8483 8.31161 20.6592 8.45721L14.9498 12.8533C14.8672 12.9169 14.8328 13.025 14.8633 13.1247L17.0382 20.2183C17.1096 20.4512 16.8396 20.6383 16.6466 20.4897L10.9919 16.1357C10.902 16.0665 10.7768 16.0665 10.6869 16.1357L5.0322 20.4897C4.83918 20.6383 4.56926 20.4512 4.64066 20.2183L6.8155 13.1247C6.84605 13.025 6.81158 12.9169 6.729 12.8533L1.01959 8.45721C0.830493 8.31161 0.933455 8.00912 1.17211 8.00912H8.19905C8.30889 8.00912 8.40587 7.93742 8.43806 7.8324L10.6004 0.779602Z"
-                                fill="#FBB040" />
-                        </svg>
+                    <svg width="21" height="21" viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M10.6004 0.779602C10.6726 0.543977 11.0062 0.543976 11.0784 0.779601L13.2408 7.8324C13.2729 7.93742 13.3699 8.00912 13.4798 8.00912H20.5067C20.7454 8.00912 20.8483 8.31161 20.6592 8.45721L14.9498 12.8533C14.8672 12.9169 14.8328 13.025 14.8633 13.1247L17.0382 20.2183C17.1096 20.4512 16.8396 20.6383 16.6466 20.4897L10.9919 16.1357C10.902 16.0665 10.7768 16.0665 10.6869 16.1357L5.0322 20.4897C4.83918 20.6383 4.56926 20.4512 4.64066 20.2183L6.8155 13.1247C6.84605 13.025 6.81158 12.9169 6.729 12.8533L1.01959 8.45721C0.830493 8.31161 0.933455 8.00912 1.17211 8.00912H8.19905C8.30889 8.00912 8.40587 7.93742 8.43806 7.8324L10.6004 0.779602Z"
+                            fill="#FBB040" />
+                    </svg>
 
-                        <svg width="21" height="21" viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path
-                                d="M10.6004 0.779602C10.6726 0.543977 11.0062 0.543976 11.0784 0.779601L13.2408 7.8324C13.2729 7.93742 13.3699 8.00912 13.4798 8.00912H20.5067C20.7454 8.00912 20.8483 8.31161 20.6592 8.45721L14.9498 12.8533C14.8672 12.9169 14.8328 13.025 14.8633 13.1247L17.0382 20.2183C17.1096 20.4512 16.8396 20.6383 16.6466 20.4897L10.9919 16.1357C10.902 16.0665 10.7768 16.0665 10.6869 16.1357L5.0322 20.4897C4.83918 20.6383 4.56926 20.4512 4.64066 20.2183L6.8155 13.1247C6.84605 13.025 6.81158 12.9169 6.729 12.8533L1.01959 8.45721C0.830493 8.31161 0.933455 8.00912 1.17211 8.00912H8.19905C8.30889 8.00912 8.40587 7.93742 8.43806 7.8324L10.6004 0.779602Z"
-                                fill="#FBB040" />
-                        </svg>
+                    <svg width="21" height="21" viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M10.6004 0.779602C10.6726 0.543977 11.0062 0.543976 11.0784 0.779601L13.2408 7.8324C13.2729 7.93742 13.3699 8.00912 13.4798 8.00912H20.5067C20.7454 8.00912 20.8483 8.31161 20.6592 8.45721L14.9498 12.8533C14.8672 12.9169 14.8328 13.025 14.8633 13.1247L17.0382 20.2183C17.1096 20.4512 16.8396 20.6383 16.6466 20.4897L10.9919 16.1357C10.902 16.0665 10.7768 16.0665 10.6869 16.1357L5.0322 20.4897C4.83918 20.6383 4.56926 20.4512 4.64066 20.2183L6.8155 13.1247C6.84605 13.025 6.81158 12.9169 6.729 12.8533L1.01959 8.45721C0.830493 8.31161 0.933455 8.00912 1.17211 8.00912H8.19905C8.30889 8.00912 8.40587 7.93742 8.43806 7.8324L10.6004 0.779602Z"
+                            fill="#FBB040" />
+                    </svg>
 
-                    </div>
-                <p class="text-[#333333] ">
-                    I bought this car from Jamex Auction House at a very low price. The quality and service are
-                    satisfying and the process of shipping is safe. My account was well-managed by the account officer
-                    of the company I am glad that I have chosen this company. Thank you very much Jamex Auto Auctions
-                    for providing me with the wonderful service and a perfect car.
-                </p>
-                <div class="text-gray-900">
-                <div class="flex gap-2">
+                </div>
+                <div class="h-[200px] text-ellipsis overflow-hidden">
+                    <p class="text-[#333333] font-Nunito font-bold">
+                        I bought this car from Jamex Auction House at a very low price. The quality and service are
+                        satisfying and the process of shipping is safe. My account was well-managed by the account
+                        officer
+                        of the company I am glad that I have chosen this company. Thank you very much Jamex Auto
+                        Auctions
+                        for providing me with the wonderful service and a perfect car.
+
+                    </p>
+                </div>
+                <div class="flex gap-2 text-gray-900 h-[50px] items-end">
                     <img class="lg:rounded-[15px] md:rounded-[10px] h-[40px] w-[40px] object-cover"
-                    src="@/assets/images/customer-feedback/avatar.svg" />
+                        src="@/assets/images/customer-feedback/avatar.svg" />
                     <div>
                         <p class="text-[16px] font-bold font-nunito">Christina</p>
                         <p class="text-[14px] font-nunito">Los Angeles, CA</p>
                     </div>
                 </div>
             </div>
+
+            <div>
+                <svg width="71" height="20" viewBox="0 0 71 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <circle cx="64.3333" cy="10" r="6.66667" transform="rotate(90 64.3333 10)" fill="#08246C"
+                        fill-opacity="0.2" />
+                    <circle cx="38.9993" cy="10" r="6.66667" transform="rotate(90 38.9993 10)" fill="#08246C"
+                        fill-opacity="0.2" />
+                    <circle cx="10.334" cy="10" r="10" transform="rotate(90 10.334 10)" fill="#08246C" />
+                    <ellipse cx="10" cy="10" rx="4" ry="4" transform="rotate(90 10 10)" fill="white" />
+                </svg>
             </div>
-        
+
+            <div class="text-white text-center font-bold font-Nunito rounded-md text-md bg-[#08246C] w-[350px] py-4">
+                See More
+            </div>
+
         </div>
     </div>
 </template>
