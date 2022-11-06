95c45e9a07bd6d42cfdca9228a177faf0d85e1e5
unknown <lakweensenathilake@gmail.com>
2022-11-06 22:32:22 +0530

    feat:why us

--- src/views/WhyUs.vue
+++ src/views/WhyUs.vue
@@ -29,37 +29,170 @@ var base_url = window.location.origin;
                 <h1 class="text-left mt-2 ml-3 text-[20px] text-[#08246C] font-bold">
                     Process of Ordering
                 </h1>
-                <div class="flex justify-start gap-3 mt-4">
-                    <div class="flex w-5 flex-col items-center ml-3">
-                        <div class="flex flex-col items-center justify-center text-white">
-                            <div
-                                class="font-Nunito flex h-6 w-6 items-center justify-center rounded-full border-2 border-[#08246C] bg-[#08246C]  text-[12px] ">
-                                1</div>
+                <div class="">
+                    <div class="flex justify-start gap-3 mt-4">
+                        <div class="flex w-5 flex-col items-center ml-3">
+                            <div class="flex flex-col items-center justify-center text-white">
+                                <div
+                                    class="font-Nunito flex h-6 w-6 items-center justify-center rounded-full border-2 border-[#08246C] bg-[#08246C]  text-[12px] ">
+                                    1</div>
+                            </div>
+                            <div class="h-[230px] flex-auto border-l-2 border-[#08246C]">
+                            </div>
                         </div>
-                        <div class="h-[179px] flex-auto border-l-2 border-[#08246C]">
+
+                        <div>
+                            <div class="text-Nunito pb-3">
+                                <div class="text-[#333333] text-left text-[16px] font-bold">
+                                    Transparent vehicle inspection with physical visit
+                                </div>
+                                <p class="text-[12px] text-black mt-1 font-medium leading-4">
+                                    Our staff members take the responsibility to visit the vehicle for physical
+                                    inspection
+                                    before placing bids on your behalf. During this visit complete inspection of the car
+                                    is
+                                    performed regarding the condition of the car. The parts of the car that are looked
+                                    at
+                                    for their condition and functionality
+                                </p>
+                                <p class="text-[12px] text-black mt-1 font-medium leading-4">
+                                    Include :
+                                </p>
+                                <div class="pl-6">
+                                    <ul class="list-disc text-[12px] text-black mt-1 font-medium leading-4">
+                                        <li>Transmission and engine</li>
+                                        <li>Bushes and suspensions</li>
+                                        <li>Power window CV Joint rubbers and central locks</li>
+                                        <li>winkers, lens and indicators</li>
+                                        <li>Driveshaft</li>
+                                        <li>Wipers Steering racks and horns</li>
+                                    </ul>
+                                </div>
+                            </div>
                         </div>
                     </div>
 
-                    <div>
-                        <div class="text-Nunito">
-                            <div class="text-[#333333] text-left text-[16px] font-bold">
-                                Transparent vehicle inspection with physical visit
+                    <div class="flex justify-start gap-3">
+                        <div class="flex w-5 flex-col items-center ml-3">
+                            <div class="flex flex-col items-center justify-center text-white">
+                                <div
+                                    class="font-Nunito flex h-6 w-6 items-center justify-center rounded-full border-2 border-[#08246C] bg-[#08246C]  text-[12px] ">
+                                    2</div>
                             </div>
-                            <p class="text-[12px] text-black mt-1 font-medium leading-4">
-                                Our staff members take the responsibility to visit the vehicle for physical inspection
-                                before placing bids on your behalf. During this visit complete inspection of the car is
-                                performed regarding the condition of the car. The parts of the car that are looked at
-                                for their condition and functionality
-                            </p>
-                            <p class="text-[12px] text-black mt-1 font-medium leading-4">
-                                Include :  
-                            </p>
-                                <ul class="text-black">
-                                    <li>
-                                        Transmission and engine 
-                                    </li>
-                                </ul>
-                           
+                            <div class="h-[230px] flex-auto border-l-2 border-[#08246C]">
+                            </div>
+                        </div>
+
+                        <div>
+                            <div class="text-Nunito">
+                                <div class="text-[#333333] text-left text-[16px] font-bold">
+                                    Process of Maintenance
+                                </div>
+                                <p class="text-[12px] text-black mt-1 leading-4">
+                                    We undertake appropriate checks to check the vehicle condition and then prepare it
+                                    for appropriate function to make it reliable for long-term use. If any malfunction
+                                    is seen in the vehicle, the shipment is done right after that.
+                                </p>
+                                <div class="pl-6">
+                                    <ul class="list-[upper-alpha] text-black mt-1 ">
+                                        <li>
+                                            <p class="text-[16px] font-semibold leading-4"> Cleaning and Coating of
+                                                Vehicle</p>
+                                            <p class="mt-3 text-[12px]">
+                                                Cleaning and coating of the car are done property from inside out. Each
+                                                and every section of the car is inspected for its looks and functioning.
+                                                Following areas are undertaken
+                                            </p>
+                                            <ul class="list-disc ml-4 text-[12px] text-black mt-1">
+                                                <li class="">
+                                                    Wax coating of the dashboard plus engine room with a clear spray
+                                                </li>
+                                                <li>
+                                                    Cleaning and steaming of the engine underframe and body
+                                                </li>
+                                                <li>
+                                                    Black paint on the chassis underbody and frame area
+                                                </li>
+
+                                            </ul>
+                                            <p class="mt-3 text-[12px]">
+                                                Maintenance and cleaning are done to assure that the car reaches the end
+                                                customer in the best condition without any issues. Also, it protects the
+                                                car from the threat of corrosion during the process of shipment.
+                                            </p>
+                                        </li>
+                                        <li class="mt-2">
+                                            <p class="text-[16px] font-semibold leading-4"> Battery and tires </p>
+                                            <p class="mt-3 text-[12px]">
+                                                Replacement of tires and batteries is done to assure the good condition
+                                                of the vehicle that can be derived on road safely.
+                                            </p>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="flex justify-start gap-3">
+                        <div class="flex w-5 flex-col items-center ml-3">
+                            <div class="flex flex-col items-center justify-center text-white">
+                                <div
+                                    class="font-Nunito flex h-6 w-6 items-center justify-center rounded-full border-2 border-[#08246C] bg-[#08246C]  text-[12px] ">
+                                    2</div>
+                            </div>
+                            <div class="h-[230px] flex-auto border-l-2 border-[#08246C]">
+                            </div>
+                        </div>
+
+                        <div>
+                            <div class="text-Nunito">
+                                <div class="text-[#333333] text-left text-[16px] font-bold">
+                                    Process of Maintenance
+                                </div>
+                                <p class="text-[12px] text-black mt-1 leading-4">
+                                    We undertake appropriate checks to check the vehicle condition and then prepare it
+                                    for appropriate function to make it reliable for long-term use. If any malfunction
+                                    is seen in the vehicle, the shipment is done right after that.
+                                </p>
+                                <div class="pl-6">
+                                    <ul class="list-[upper-alpha] text-black mt-1 ">
+                                        <li>
+                                            <p class="text-[16px] font-semibold leading-4"> Cleaning and Coating of
+                                                Vehicle</p>
+                                            <p class="mt-3 text-[12px]">
+                                                Cleaning and coating of the car are done property from inside out. Each
+                                                and every section of the car is inspected for its looks and functioning.
+                                                Following areas are undertaken
+                                            </p>
+                                            <ul class="list-disc ml-4 text-[12px] text-black mt-1">
+                                                <li class="">
+                                                    Wax coating of the dashboard plus engine room with a clear spray
+                                                </li>
+                                                <li>
+                                                    Cleaning and steaming of the engine underframe and body
+                                                </li>
+                                                <li>
+                                                    Black paint on the chassis underbody and frame area
+                                                </li>
+
+                                            </ul>
+                                            <p class="mt-3 text-[12px]">
+                                                Maintenance and cleaning are done to assure that the car reaches the end
+                                                customer in the best condition without any issues. Also, it protects the
+                                                car from the threat of corrosion during the process of shipment.
+                                            </p>
+                                        </li>
+                                        <li class="mt-2">
+                                            <p class="text-[16px] font-semibold leading-4"> Battery and tires </p>
+                                            <p class="mt-3 text-[12px]">
+                                                Replacement of tires and batteries is done to assure the good condition
+                                                of the vehicle that can be derived on road safely.
+                                            </p>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
