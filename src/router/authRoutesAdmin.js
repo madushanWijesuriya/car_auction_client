68b45ba26eb13ddcc6ff65b7ee9dbc240d4444a9
Madushan Wijesuriya <67863463+madushanWijesuriya@users.noreply.github.com>
2022-11-15 23:53:54 +0530

    fix build error

--- src/router/authRoutesAdmin.js
+++ src/router/authRoutesAdmin.js
@@ -4,13 +4,13 @@ const authRoutesAdmin = [
   {
     path: '/login',
     name: 'login-admin',
-    component: () => import('../views/admin/LoginIn.vue'),
+    component: () => import('@/views/admin/LoginIn.vue'),
     meta: { requiresAuth: false, layout: 'LayoutAdmin' },
   },
   {
     path: '/secured-route',
     name: 'securedRoute',
-    component: () => import('../views/admin/TestSecured.vue'),
+    component: () => import('@/views/admin/TestSecured.vue'),
     meta: { requiresAuth: true, layout: 'LayoutAdmin' },
   },
   {
