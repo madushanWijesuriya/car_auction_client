e2a453b0172e6db3cda0c7e09eed29e7f8b7bf1e
Thenukad Dinu <thenukadinu@gmail.com>
2022-11-20 00:32:20 +0530

    add car backend call bind

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup>
-import { RouterLink, RouterView } from "vue-router";
-import Navbar from "@/components/layout/Navbar.vue";
-import Menubar from "@/components/layout/TopMenu.vue";
-import Footer from "@/components/layout/Footer.vue";
-import AdminAuthenticated from "./components/layout/admin/LayoutAuthenticated.vue";
+import { RouterLink, RouterView } from 'vue-router'
+import Navbar from '@/components/layout/Navbar.vue'
+import Menubar from '@/components/layout/TopMenu.vue'
+import Footer from '@/components/layout/Footer.vue'
+import AdminAuthenticated from './components/layout/admin/LayoutAuthenticated.vue'
 </script>
 
 <template>
