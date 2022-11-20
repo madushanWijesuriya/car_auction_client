9d50a08171ee90acbb807a9fa9939e8cf350df78
unknown <lakweensenathilake@gmail.com>
2022-11-20 22:46:35 +0530

    fix

--- src/components/StockList/VehicalList/components/VehicalDetailCard.vue
+++ src/components/StockList/VehicalList/components/VehicalDetailCard.vue
@@ -1,7 +1,7 @@
 <script setup>
 </script>
 <template>
-    <div class="w-full text-black p-2 rounded-md border space-y-3 border-[#E7E7E7] md:flex md:justify-between md:gap-2">
+    <div class="w-full text-black p-2 rounded-md border space-y-3 border-[#E7E7E7] lg:flex lg:justify-between lg:gap-2">
         <img class="lg:rounded-[15px] md:rounded-[10px] w-full md:w-[25vw] object-cover"
             src="@/assets/images/stock-list/vehical-list/list-car-1.svg" />
 
@@ -9,7 +9,7 @@
             <div class=" text-center">
                 Toyota Mark II E-JZX100 1997/08
             </div>
-            <div class="flex flex-wrap gap-4 w-full justify-between md:justify-start">
+            <div class="flex flex-wrap gap-4 w-full justify-between lg:justify-start">
                 <div class="">
                     <p>Engine Capacity</p>
                     <p>2,490 CC</p>
@@ -28,24 +28,24 @@
                 </div>
             </div>
             <div class="flex flex-wrap text-white text-[10px] gap-2 mb-auto">
-                <div class=" px-5 py-1 bg-[#08246C] rounded-lg text-center grow md:flex-none">
+                <div class=" px-5 py-1 bg-[#08246C] rounded-lg text-center grow lg:flex-none">
                     Petrol
                 </div>
-                <div class=" px-5 py-1 bg-[#08246C] rounded-lg text-center grow md:flex-none">
+                <div class=" px-5 py-1 bg-[#08246C] rounded-lg text-center grow lg:flex-none">
                     Auto Gear
                 </div>
-                <div class=" px-5 py-1 bg-[#08246C] rounded-lg text-center grow md:flex-none">
+                <div class=" px-5 py-1 bg-[#08246C] rounded-lg text-center grow lg:flex-none">
                     Right Hand
                 </div>
-                <div class=" px-5 py-1 bg-[#08246C] rounded-lg text-center grow md:flex-none">
+                <div class=" px-5 py-1 bg-[#08246C] rounded-lg text-center grow lg:flex-none">
                     4WD
                 </div>
             </div>
         </div>
-        <div class="w-full px-2 md:w-[25%] space-y-2">
-            <div class="flex md:flex-col w-full justify-between md:items-end">
+        <div class="w-full px-2 lg:w-[25%] space-y-2">
+            <div class="flex lg:flex-col w-full justify-between lg:items-end">
                 <div class="w-full flex-col md:items-end">
-                    <div class="flex flex-col md:flex-row md:justify-between">
+                    <div class="flex flex-col md:flex-row lg:justify-between">
                         <p class="">
                             Car Price :
                         </p>
@@ -54,13 +54,13 @@
                         </p>
                     </div>
 
-                    <div class="flex w-full md:justify-end items-start">
+                    <div class="flex w-full lg:justify-end items-start">
 
                         Sri Lanka-Hambantota
                     </div>
 
                 </div>
-                <div class="flex w-full flex-col md:items-end">
+                <div class="flex w-full flex-col lg:items-end">
                     <p class="">
                         ¥ 176,230.00
                     </p>
@@ -68,9 +68,8 @@
                         Total Price with Shipping
                     </p>
                 </div>
-
             </div>
-            <div class="bg-[#08246C] mt-14 text-center py-2  text-white rounded-md px-4 ">
+            <div class="bg-[#08246C] mt-auto text-center py-2  text-white rounded-md px-4 ">
                 + Add to Wishlist
             </div>
         </div>
