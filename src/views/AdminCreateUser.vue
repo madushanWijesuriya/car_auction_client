df550af0b3789aec9ad77bbca7e96838f68a7a83
Madushan Wijesuriya <67863463+madushanWijesuriya@users.noreply.github.com>
2022-12-07 23:04:36 +0530

    Update AdminCreateUser.vue

--- src/views/AdminCreateUser.vue
+++ src/views/AdminCreateUser.vue
@@ -12,92 +12,136 @@ import BaseButton from '@/components/admin/BaseButton.vue'
 import httpResource from '@/http/httpResource'
 
 export default {
-    setup() {
-        const validateForm = () => {
-            // handle frontend validations
-            submitForm()
-        }
-
-        const resetForm = () => {
-            Object.assign(form, initialState)
-            uploaderKey.value += uploaderKey.value + 1
-        }
-
-        const submitForm = async () => {
-            try {
-
-                console.log(form, 'form');
-                const response = await httpResource.post('/api/staff/staffuser', {
-                    name: form?.name,
-                    email: form?.email,
-                    password: form?.password,
-                    password_confirmation: form?.password_confirmation,
-                    role_id: form?.role_id.id
-                })
-                if (response.status === 200) {
-                    resetForm()
-                }
-            } catch (error) {
-                console.error(error?.response?.data?.message)
-            }
-        }
-
-        let roleIds = ref([{ id:1,label:'Admin'},{id:2 , label:'Test'}])
-        const initialState = {
-            name: '',
-            email: null,
-            password: '',
-            role_id: '1',
-            password_confirmation: ''
-        }
-
-        let form = reactive({ ...initialState })
-        return {
-            form,
-            roleIds,
-            validateForm,
-            resetForm,
-        }
+  setup() {
+    const validateForm = () => {
+      // handle frontend validations
+      submitForm()
     }
+
+    const resetForm = () => {
+      Object.assign(form, initialState)
+      uploaderKey.value += uploaderKey.value + 1
+    }
+
+    const submitForm = async () => {
+      try {
+        console.log(form, 'form')
+        const response = await httpResource.post('/api/staff/staffuser', {
+          name: form?.name,
+          email: form?.email,
+          password: form?.password,
+          password_confirmation: form?.password_confirmation,
+          role_id: form?.role_id.id,
+        })
+        if (response.status === 200) {
+          resetForm()
+        }
+      } catch (error) {
+        console.error(error?.response?.data?.message)
+      }
+    }
+
+    let roleList = ref([])
+    const getRoles = async () => {
+      try {
+        const response = await httpResource.get('/api/resources/roles')
+        roleList.value = response.data.data.map((d) => ({
+          ...d,
+          label: d.name,
+        }))
+      } catch (error) {}
+    }
+    const initialState = {
+      name: '',
+      email: null,
+      password: '',
+      role_id: roleList[0],
+      password_confirmation: '',
+    }
+
+    let form = reactive({ ...initialState })
+
+    onMounted(() => {
+      getRoles()
+    })
+
+    return {
+      form,
+      validateForm,
+      resetForm,
+      roleList,
+    }
+  },
 }
 </script>
 <template>
-    <div class="add-car">
-        <LayoutAuthenticated>
-            <SectionMain>
-                <CardBox form @submit.prevent="submit">
-                    <SectionTitleLineWithButton :icon="mdiCarEstate" title="Add Staff User" main>
-                    </SectionTitleLineWithButton>
-                    <FormField label="Name" help="">
-                        <FormControl v-model="form.name" type="text" placeholder="User Name" />
-                    </FormField>
-                    <FormField label="Email">
-                        <FormControl v-model="form.email" type="email" placeholder="Email" />
-                    </FormField>
-                    <FormField label="Password" help="">
-                        <FormControl v-model="form.password" type="password" placeholder="" />
-                    </FormField>
-                    <FormField label="Confirm Password">
-                        <FormControl v-model="form.password_confirmation" name="password_confirmation" type="Password"
-                            placeholder="" />
-                    </FormField>
-                    <FormField label="Role">
-                        <FormControl v-model="form.role_id" :options="roleIds" />
-                    </FormField>
-                </CardBox>
-            </SectionMain>
-            <SectionMain>
-                <CardBox>
-                    <template #footer>
-                        <BaseButtons>
-                            <BaseButton type="submit" color="info" label="Submit" @click="validateForm" />
-                            <BaseButton type="reset" color="info" outline label="Reset" @click="resetForm" />
-                        </BaseButtons>
-                    </template>
-                </CardBox>
-            </SectionMain>
+  <div class="add-car">
+    <LayoutAuthenticated>
+      <SectionMain>
+        <CardBox form @submit.prevent="submit">
+          <SectionTitleLineWithButton
+            :icon="mdiCarEstate"
+            title="Add Staff User"
+            main
+          >
+          </SectionTitleLineWithButton>
+          <FormField label="Name" help="">
+            <FormControl
+              v-model="form.name"
+              type="text"
+              placeholder="User Name"
+            />
+          </FormField>
+          <FormField label="Email">
+            <FormControl
+              v-model="form.email"
+              type="email"
+              placeholder="Email"
+            />
+          </FormField>
+          <FormField label="Password" help="">
+            <FormControl
+              v-model="form.password"
+              type="password"
+              placeholder=""
+            />
+          </FormField>
+          <FormField label="Confirm Password">
+            <FormControl
+              v-model="form.password_confirmation"
+              name="password_confirmation"
+              type="Password"
+              placeholder=""
+            />
+          </FormField>
+          <FormField label="Role">
+            <FormControl v-model="form.role_id" :options="roleList" />
+          </FormField>
+        </CardBox>
+      </SectionMain>
+      <SectionMain>
+        <CardBox>
+          <template #footer>
+            <BaseButtons>
+              <BaseButton
+                type="submit"
+                color="info"
+                label="Submit"
+                @click="validateForm"
+              />
+              <BaseButton
+                type="reset"
+                color="info"
+                outline
+                label="Reset"
+                @click="resetForm"
+              />
+            </BaseButtons>
+          </template>
+        </CardBox>
+      </SectionMain>
 
-            <BaseDivider />
-        </LayoutAuthenticated>
-    </div>
+      <BaseDivider />
+    </LayoutAuthenticated>
+  </div>
 </template>
