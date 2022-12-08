e3d5e4893fbc0dba57b8db30fe0cd3d0c796d88a
Madushan Wijesuriya <67863463+madushanWijesuriya@users.noreply.github.com>
2022-12-08 19:26:55 +0530

    quick add model and toast message

---
+++ src/components/admin/modals/AddModal.vue
@@ -0,0 +1,37 @@
+<template>
+  <QuickAddForm
+    v-model="state.dialog"
+    title="Please confirm action"
+    button-label="Confirm"
+    has-cancel
+  >
+    
+  </QuickAddForm>
+</template>
+<script>
+import QuickAddForm from '@/components/admin/modals/QuickAddForm.vue'
+import { reactive } from '@vue/reactivity'
+
+export default {
+  components: {
+    QuickAddForm,
+  },
+  setup(props) {
+    const state = reactive({ dialog: false })
+
+    // expose the state to the template
+    return {
+      state,
+    }
+  },
+
+  methods: {
+    openModal() {
+      this.state.dialog = true
+    },
+    closeModal() {
+      this.state.dialog = false
+    },
+  },
+}
+</script>
