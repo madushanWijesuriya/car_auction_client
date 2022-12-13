1892c6a875da8893ac6726963c211b88dff84220
Madushan Wijesuriya <67863463+madushanWijesuriya@users.noreply.github.com>
2022-12-14 01:02:26 +0530

    remove nav bar

--- src/js/menuNavBar.js
+++ src/js/menuNavBar.js
@@ -13,63 +13,63 @@ import {
 } from '@mdi/js'
 
 export default [
-  {
-    icon: mdiMenu,
-    label: 'Sample menu',
-    menu: [
-      {
-        icon: mdiClockOutline,
-        label: 'Item One',
-      },
-      {
-        icon: mdiCloud,
-        label: 'Item Two',
-      },
-      {
-        isDivider: true,
-      },
-      {
-        icon: mdiCrop,
-        label: 'Item Last',
-      },
-    ],
-  },
-  {
-    isCurrentUser: true,
-    menu: [
-      {
-        icon: mdiAccount,
-        label: 'My Profile',
-        to: '/admin/profile',
-      },
-      {
-        icon: mdiCogOutline,
-        label: 'Settings',
-      },
-      {
-        icon: mdiEmail,
-        label: 'Messages',
-      },
-      {
-        isDivider: true,
-      },
-      {
-        icon: mdiLogout,
-        label: 'Log Out',
-        isLogout: true,
-      },
-    ],
-  },
-  {
-    icon: mdiThemeLightDark,
-    label: 'Light/Dark',
-    isDesktopNoLabel: true,
-    isToggleLightDark: true,
-  },
-  {
-    icon: mdiLogout,
-    label: 'Log out',
-    isDesktopNoLabel: true,
-    isLogout: true,
-  },
+  // {
+  //   icon: mdiMenu,
+  //   label: 'Sample menu',
+  //   menu: [
+  //     {
+  //       icon: mdiClockOutline,
+  //       label: 'Item One',
+  //     },
+  //     {
+  //       icon: mdiCloud,
+  //       label: 'Item Two',
+  //     },
+  //     {
+  //       isDivider: true,
+  //     },
+  //     {
+  //       icon: mdiCrop,
+  //       label: 'Item Last',
+  //     },
+  //   ],
+  // },
+  // {
+  //   isCurrentUser: true,
+  //   menu: [
+  //     {
+  //       icon: mdiAccount,
+  //       label: 'My Profile',
+  //       to: '/admin/profile',
+  //     },
+  //     {
+  //       icon: mdiCogOutline,
+  //       label: 'Settings',
+  //     },
+  //     {
+  //       icon: mdiEmail,
+  //       label: 'Messages',
+  //     },
+  //     {
+  //       isDivider: true,
+  //     },
+  //     {
+  //       icon: mdiLogout,
+  //       label: 'Log Out',
+  //       isLogout: true,
+  //     },
+  //   ],
+  // },
+  // {
+  //   icon: mdiThemeLightDark,
+  //   label: 'Light/Dark',
+  //   isDesktopNoLabel: true,
+  //   isToggleLightDark: true,
+  // },
+  // {
+  //   icon: mdiLogout,
+  //   label: 'Log out',
+  //   isDesktopNoLabel: true,
+  //   isLogout: true,
+  // },
 ]
