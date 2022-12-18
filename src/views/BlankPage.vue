54529f6b5866f1a65be469c2de86f453e6dc574b
Udaya De Zoysa <unzoysa.un@gmail.com>
2022-12-18 14:54:39 +0530

    stock list changes

--- src/views/BlankPage.vue
+++ src/views/BlankPage.vue
@@ -1,13 +1,18 @@
 <script setup>
 import Filter from '../components/Filter.vue';
+import NewVehicalList from '../components/StockList/VehicalList/NewVehicalList.vue';
 </script>
 
 <template>
   <div class="lg:container mt-8 mb-8 lg:mx-auto lg:px-20">
     <div class="grid grid-cols-1 lg:grid-cols-[300px_minmax(600px,_1fr)_300px]">
-      <div><Filter></Filter></div>
+      <div>
+        <Filter></Filter>
+      </div>
+      <div>
+        <NewVehicalList></NewVehicalList>
+      </div>
       <div></div>
-      <div>03</div>
     </div>
   </div>
 </template>
