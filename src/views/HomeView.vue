9efcfb8ad9c6e0e5061b7a4604e6494d7143145f
Thenukad Dinu <thenukadinu@gmail.com>
2022-12-24 01:53:43 +0530

    add missing sections to home page.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,6 +5,7 @@ import FilterBox from '../components/FilterBox.vue'
 import CardTwo from '../components/Cards/CardTwo.vue'
 import CardOne from '../components/Cards/CardOne.vue'
 import ButtoneTwo from '../components/Buttons/ButtonTwo.vue'
+import RatingsGroup from '../components/RatingsGroup.vue'
 </script>
 
 <template>
@@ -31,11 +32,15 @@ import ButtoneTwo from '../components/Buttons/ButtonTwo.vue'
         <div class="grid place-items-center color-02 tracking-5 font-bold">
           WHAT WE DO
         </div>
-        <div class="grid place-items-center color-01 font-bold tracking-4 text-2xl sm:text-5xl">
+        <div
+          class="grid place-items-center color-01 font-bold tracking-4 text-2xl sm:text-5xl"
+        >
           Why Choose Us?
         </div>
       </div>
-      <div class="content-02 text-sm sm:text-2xl grid place-items-center mt-3 sm:mt-11">
+      <div
+        class="content-02 text-sm sm:text-2xl grid place-items-center mt-3 sm:mt-11"
+      >
         <div class="container px-4 sm:px-8 md:px-36 text-center color-04">
           We are a strong member of all Japanese Auction houses and hence will
           assure you the purchase of the most useful cars with cost-saving.
@@ -45,39 +50,81 @@ import ButtoneTwo from '../components/Buttons/ButtonTwo.vue'
       </div>
       <div class="content-03">
         <div class="container">
-          <div class="grid gap-6 sm:gap-20 grid-cols-1 sm:grid-cols-2 md:grid-cols-4 px-8 mt-7 sm:mt-20">
-            <CardTwo :iconPath="'path'" :title="'Low Pricing'" data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-duration="500"
-              :content="'Japanese cars from Auction House Japan are cheaper as compared to the other exporters as we do not take a huge cost of car keeping in the yard.'">
+          <div
+            class="grid gap-6 sm:gap-20 grid-cols-1 sm:grid-cols-2 md:grid-cols-4 px-8 mt-7 sm:mt-20"
+          >
+            <CardTwo
+              :iconPath="'path'"
+              :title="'Low Pricing'"
+              data-aos="fade-up"
+              data-aos-anchor-placement="top-bottom"
+              data-aos-duration="500"
+              :content="'Japanese cars from Auction House Japan are cheaper as compared to the other exporters as we do not take a huge cost of car keeping in the yard.'"
+            >
               <template v-slot:icon>
-                <img src="@/assets/images/icons/card-icon-01.svg" alt="icon-01" />
+                <img
+                  src="@/assets/images/icons/card-icon-01.svg"
+                  alt="icon-01"
+                />
               </template>
             </CardTwo>
-            <CardTwo :iconPath="'path'" :title="'Safety'" data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-duration="1000"
-              :content="'The vehicle trading market in Japan is the most trustworthy all over the world because exporters are accountable for their trading'">
+            <CardTwo
+              :iconPath="'path'"
+              :title="'Safety'"
+              data-aos="fade-up"
+              data-aos-anchor-placement="top-bottom"
+              data-aos-duration="1000"
+              :content="'The vehicle trading market in Japan is the most trustworthy all over the world because exporters are accountable for their trading'"
+            >
               <template v-slot:icon>
-                <img src="@/assets/images/icons/card-icon-02.svg" alt="icon-02" />
+                <img
+                  src="@/assets/images/icons/card-icon-02.svg"
+                  alt="icon-02"
+                />
               </template>
             </CardTwo>
-            <CardTwo :iconPath="'path'" :title="'Transparent deals'" data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-duration="1500"
-              :content="'You will get the auction and inspection sheet before placing the bid to assure maximum transparency'">
+            <CardTwo
+              :iconPath="'path'"
+              :title="'Transparent deals'"
+              data-aos="fade-up"
+              data-aos-anchor-placement="top-bottom"
+              data-aos-duration="1500"
+              :content="'You will get the auction and inspection sheet before placing the bid to assure maximum transparency'"
+            >
               <template v-slot:icon>
-                <img src="@/assets/images/icons/card-icon-03.svg" alt="icon-03" />
+                <img
+                  src="@/assets/images/icons/card-icon-03.svg"
+                  alt="icon-03"
+                />
               </template>
             </CardTwo>
-            <CardTwo :iconPath="'path'" :title="'More options'" data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-duration="2000"
-              :content="'Duis aute irure dolor reprehen derit in voluptat velit.'">
+            <CardTwo
+              :iconPath="'path'"
+              :title="'More options'"
+              data-aos="fade-up"
+              data-aos-anchor-placement="top-bottom"
+              data-aos-duration="2000"
+              :content="'Duis aute irure dolor reprehen derit in voluptat velit.'"
+            >
               <template v-slot:icon>
-                <img src="@/assets/images/icons/card-icon-04.svg" alt="icon-04" />
+                <img
+                  src="@/assets/images/icons/card-icon-04.svg"
+                  alt="icon-04"
+                />
               </template>
             </CardTwo>
           </div>
         </div>
       </div>
       <div class="content-04 container">
-        <div class="grid place-items-center color-01 text-2xl sm:text-5xl font-bold mt-9 sm:mt-16">
+        <div
+          class="grid place-items-center color-01 text-2xl sm:text-5xl font-bold mt-9 sm:mt-16"
+        >
           New Arrival
         </div>
-        <div class="grid gap-6 sm:gap-16 grid-cols-1 sm:grid-cols-2 md:grid-cols-4 mt-6 sm:mt-12 place-items-center">
+        <div
+          class="grid gap-6 sm:gap-16 grid-cols-1 sm:grid-cols-2 md:grid-cols-4 mt-6 sm:mt-12 place-items-center"
+        >
           <!-- <CardOne />
           <CardOne />
           <CardOne />
@@ -86,16 +133,22 @@ import ButtoneTwo from '../components/Buttons/ButtonTwo.vue'
       </div>
       <div class="content-05 container mt-9 px-4 md:px-20">
         <div class="grid grid-cols-1 md:grid-cols-2 gap-0 md:gap-16">
-          <div class="color-01 text-2xl text-center font-bold leading-6 mb-10 md:col-span-2">
+          <div
+            class="color-01 text-2xl text-center font-bold leading-6 mb-10 md:col-span-2"
+          >
             Biggest Vehicle Trading company of Japan
           </div>
           <div data-aos="fade-right">
             <div class="shade-window bg-color-05"></div>
-            <img src="@/assets/images/home/asset_001.jpg"
-              alt="photo-two-smart-elegant-men-which-have-deal-about-car-they-are-doing-handshake" />
+            <img
+              src="@/assets/images/home/asset_001.jpg"
+              alt="photo-two-smart-elegant-men-which-have-deal-about-car-they-are-doing-handshake"
+            />
           </div>
           <div data-aos="fade-left">
-            <div class="leading-5 mt-9 text-sm md:text-2xl md:leading-8 color-04">
+            <div
+              class="leading-5 mt-9 text-sm md:text-2xl md:leading-8 color-04"
+            >
               Jamex Co LTD is the leading auto trading company of Japan that is
               working in the same niche for over 10 years. We have satisfied
               customers in different parts of the world including Europe
@@ -108,6 +161,55 @@ import ButtoneTwo from '../components/Buttons/ButtonTwo.vue'
               <ButtoneTwo text="Read More" />
             </div>
           </div>
+        </div>
+      </div>
+      <div class="contenet-6 container mt-9 md:mt-20">
+        <div
+          data-aos="fade-right"
+          class="color-01 font-size-24 md:font-size-48 font-bold text-center"
+        >
+          Our FREE Services
+        </div>
+        <div
+          data-aos="fade-right"
+          class="mt-14 flex flex-wrap gap-3 md:gap-10 font-size-16 md:font-size-32 justify-center leading-6 md:leading-10"
+        >
+          <span class="flex"
+            >Documentation
+            <span class="ml-5 color-02"> | </span>
+          </span>
+          <span class="flex"
+            >Courier
+            <span class="ml-5 color-02"> |</span>
+          </span>
+          <span class="flex">
+            No Tax
+            <span class="ml-5 color-02">|</span>
+          </span>
+          <span class="flex"
+            >Auction Sheet
+            <span class="ml-5 color-02">|</span>
+          </span>
+          <span class="flex"
+            >Transaltion
+            <span class="ml-5 color-02">|</span>
+          </span>
+          <span>No Recycle Fee</span>
+        </div>
+        <div
+          data-aos="fade-left"
+          class="color-01 font-bold font-size-24 md:font-size-48 mt-9 md:mt-20 text-center leading-7 md:leading-10"
+        >
+          What's our client say about us
+        </div>
+        <RatingsGroup />
+        <div class="mt-6 text-center">
+          <button
+            type="button"
+            class="w-full lg:w-44 text-white bg-color-01 font-size-20 rounded-lg py-3 hover:bg-[#0f2869] shadow-md"
+          >
+            See More
+          </button>
         </div>
       </div>
     </div>
