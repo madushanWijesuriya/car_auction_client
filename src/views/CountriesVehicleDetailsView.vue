a055ad3270771f1739562f7818f9788c41c07e51
Thenukad Dinu <thenukadinu@gmail.com>
2022-12-25 19:37:28 +0530

    image issues fix - country details page.

--- src/views/CountriesVehicleDetailsView.vue
+++ src/views/CountriesVehicleDetailsView.vue
@@ -69,11 +69,7 @@ onMounted(() => {
       >
         <img
           class="rounded-md shadow"
-          :src="
-            getImageUrl(
-              '../assets/images/countries-details/car-01-counties.jpg'
-            )
-          "
+          src="@\assets\images\countries-details\car-01-counties.jpg"
           alt="car-01-counties-details"
         />
       </div>
@@ -115,7 +111,7 @@ onMounted(() => {
         class="flex justify-center mt-6 order-6 sm:absolute sm:right-2/4 sm:bottom-0"
       >
         <img
-          :src="getImageUrl('../assets/images/countries-details/QR-code.jpg')"
+          src="@\assets\images\countries-details\QR-code.jpg"
           alt="QR-CODE"
         />
       </div>
