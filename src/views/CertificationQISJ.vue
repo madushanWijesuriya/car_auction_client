43b36961d0a050dc61ccf433eaa3cddf3f576518
unknown <lakweensenathilake@gmail.com>
2022-12-27 21:25:50 +0530

    certifications

--- src/views/CertificationQISJ.vue
+++ src/views/CertificationQISJ.vue
@@ -8,71 +8,73 @@
         </div>
         <div class="certificate-heading">
             <h2>Jamex Auto Auction</h2>
-            <h4 class="certification-caption">Provides Inspection and Certification from VCA</h4>
+            <h4 class="certification-caption">Provides Inspection and Certification from QISJ</h4>
         </div>
     </div>
     <div class="m-6 lg:m-28">
         <div class="ship-service-card">
-            <h1 class="h1 text-dark text-5xl">About <span class="text-red-600"> VCA </span></h1>
+            <h1 class="h1 text-dark text-5xl">About <span class="text-red-600">QISJ</span></h1>
             <div class="certification-para-text pt-1 pb-2">
-                VCA is operating as an executive agency in the department of Department of Transport of the United
-                Kingdom. The company is involved in the provision of approval certificates
-                for different used vehicles including those in North America, Korea, Malaysia, China, the UK, Japan,
-                Brazil Australia, Italy, and India.
+                QISJ is the Quality Inspection Services Japan that offers its services for vehicle inspection for used
+                cars in Japan. The headquarter of QISJ is located in Japan but along with that the company is also
+                operating its business in South Africa, the United Kingdom Singapore, and United Arab, Emirates. The
+                company maintains a distinguished position in the market as it carries inspection services with a
+                qualified team consisting of around 200 professionals experienced in the area of car inspection.
+            </div>
+            <div class="img-on-para pt-10">
+                <img src="@/assets/images/certifications/qisj-1.png" alt="">
+            </div>
+            <h1 class="h1 text-dark text-5xl">Vehical Inspection <span class="text-red-600">QISJ</span></h1>
+            <div class="certification-para-text pt-1 pb-2">
+                QISJ Quality Inspection Services Japan undertakes the process of certification and inspection for more
+                than 150 safety checks. The inspection experts undertake these checks and prepare reports. They also
+                provide a certificate for vehicles that successfully qualify their process of inspection as per the
+                given standards.
 
-                <div class="img-on-para">
-                    <img src="@/assets/images/certifications/image-62.png" alt="">
-                </div>
+                <h1 class=" font-bold pt-2">
+                    Vehicle inspections at QISJ include:
+                </h1>
+
             </div>
-            <h1 class="h1 text-dark text-5xl">Vehical Inspection <span class="text-red-600"> VCA </span></h1>
-            <div class="certification-para-text pt-1 pb-2">
-                VCA inspection is undertaken with a detailed check of the performance of the vehicle. It is assured that
-                the specifications are as per the requirements of the rules of the destination country.
-                <div class="img-on-para">
-                    <img src="@/assets/images/certifications/image-62.png" alt="">
-                </div>
-            </div>
+            <!-- <div class="img-on-para pt-10">
+                <img src="@/assets/images/certifications/image-62.png" alt="">
+            </div> -->
             <div class="certification-list p-4 text-2xl grid grid-cols-1 gap-1 md:grid-cols-2">
                 <div class="ul-list">
                     <ul class="list-none">
                         <li class="flex justify-start items-center"><img class="p-2"
-                                src="@/assets/images/certifications/icons/charm_circle-tick.svg" alt=""> Fuel
-                            Consumption and CO2 emission </li>
+                                src="@/assets/images/certifications/icons/charm_circle-tick.svg" alt="">Cosmetic, paint,
+                            dents, cleanliness (External and Internal)</li>
                         <li class="flex justify-start items-center"><img class="p-2"
-                                src="@/assets/images/certifications/icons/charm_circle-tick.svg" alt=""> Import of
-                            vehicle
+                                src="@/assets/images/certifications/icons/charm_circle-tick.svg" alt="">All Electrical
+                            Elements checking</li>
+                        <li class="flex justify-start items-center"><img class="p-2"
+                                src="@/assets/images/certifications/icons/charm_circle-tick.svg" alt="">Radiation tests
+                            of vehicle</li>
+                        <li class="flex justify-start items-center"><img class="p-2"
+                                src="@/assets/images/certifications/icons/charm_circle-tick.svg" alt="">Checking of
+                            Electrical and Mechanical Functions</li>
+                        <li class="flex justify-start items-center"><img class="p-2"
+                                src="@/assets/images/certifications/icons/charm_circle-tick.svg" alt="">Certification
+                            and Inspection
                         </li>
                         <li class="flex justify-start items-center"><img class="p-2"
-                                src="@/assets/images/certifications/icons/charm_circle-tick.svg" alt=""> Rules and
-                            regulations
-                        </li>
+                                src="@/assets/images/certifications/icons/charm_circle-tick.svg" alt="">Roadworthiness
+                            and safety</li>
                         <li class="flex justify-start items-center"><img class="p-2"
-                                src="@/assets/images/certifications/icons/charm_circle-tick.svg" alt="">Single Vehicle
-                            Approval SVA is developed following the government of Malta for imports
-                        </li>
-                        <li class="flex justify-start items-center"><img class="p-2"
-                                src="@/assets/images/certifications/icons/charm_circle-tick.svg" alt="">
-                                Malta is the country where VCA is recommended for inspection
-                        </li>
+                                src="@/assets/images/certifications/icons/charm_circle-tick.svg" alt="">QISJ is
+                            recommended for Kenya</li>
                     </ul>
                 </div>
                 <div class="car-option">
-                    <img src="@/assets/images/certifications/image63.png" alt="">
+                    <img src="@/assets/images/certifications/eaa-2.png" alt="">
                 </div>
             </div>
         </div>
-        <div class="countries justify-items-center rows-5 md:columns-5 pt-10">
-            <div class="country text-center w-full">
+        <div class="countries justify-items-center columns-1 pt-10">
+            <div class="flex flex-col md:flex-row text-center w-full justify-start items-start gap-5">
                 <img src="@/assets/images/certifications/image-64.png" alt="">
-                <h1 class="pt-5 font-bold leading-10 text-3xl text-center">New Zealand</h1>
-                <img src="@/assets/images/certifications/image-64.png" alt="">
-                <h1 class="pt-5 font-bold leading-10 text-3xl text-center">Fiji</h1>
-                <img src="@/assets/images/certifications/image-64.png" alt="">
-                <h1 class="pt-5 font-bold leading-10 text-3xl text-center">Malta</h1>
-                <img src="@/assets/images/certifications/image-64.png" alt="">
-                <h1 class="pt-5 font-bold leading-10 text-3xl text-center">Uganda</h1>
-                <img src="@/assets/images/certifications/image-64.png" alt="">
-                <h1 class="pt-5 font-bold leading-10 text-3xl text-center">Zambia</h1>
+                <h1 class="pt-5 font-bold leading-10 text-3xl text-center">Kenya</h1>
             </div>
         </div>
         <div class="disclaimer-text ">
